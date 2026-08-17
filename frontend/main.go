9f8650f1f78dd4c976463d1eacdf886c991c4a30
Casper F <cafis17@student.sdu.dk>
2026-08-17 10:17:11 +0000

    Fixes last linter errors frfr this time

--- frontend/main.go
+++ frontend/main.go
@@ -29,49 +29,68 @@ var myClient = &http.Client{Timeout: 10 * time.Second}
 
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    io.WriteString(w, "healthy")
+    if _, err := io.WriteString(w, "healthy"); err != nil {
+        log.Println("failed to write health response:", err)
+}
 }
 
 func main() {
 
     http.HandleFunc("/healthz", HealthzHandler)
 
-    http.HandleFunc("/api/random", func (w http.ResponseWriter, r *http.Request) {
-        resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes/random", BACKEND_DNS, BACKEND_PORT))
+    http.HandleFunc("/api/random", func(w http.ResponseWriter, r *http.Request) {
+        resp, err := myClient.Get(fmt.Sprintf(
+            "http://%s:%s/fortunes/random",
+            BACKEND_DNS,
+            BACKEND_PORT,
+        ))
         if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
+            log.Println(err)
+            http.Error(w, "backend unavailable", http.StatusBadGateway)
             return
         }
-
+    
         f := new(fortune)
-        json.NewDecoder(resp.Body).Decode(f)
-
-        fmt.Fprint(w, f.Message)
-        return
+        if err := json.NewDecoder(resp.Body).Decode(f); err != nil {
+            log.Println("failed to decode backend response:", err)
+            http.Error(w, "invalid response from backend", http.StatusBadGateway)
+            return
+        }
+    
+        if _, err := fmt.Fprint(w, f.Message); err != nil {
+            log.Println("failed to write response:", err)
+        }
     })
 
-    http.HandleFunc("/api/all", func (w http.ResponseWriter, r *http.Request) {
-        resp, err := myClient.Get(fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT))
+    http.HandleFunc("/api/all", func(w http.ResponseWriter, r *http.Request) {
+        resp, err := myClient.Get(fmt.Sprintf(
+            "http://%s:%s/fortunes",
+            BACKEND_DNS,
+            BACKEND_PORT,
+        ))
         if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
+            log.Println(err)
+            http.Error(w, "backend unavailable", http.StatusBadGateway)
             return
         }
-
+    
         fortunes := new([]fortune)
-        json.NewDecoder(resp.Body).Decode(fortunes)
-
-        tmpl, err := template.ParseFiles("./templates/fortunes.html")
-
-        if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
+        if err := json.NewDecoder(resp.Body).Decode(fortunes); err != nil {
+            log.Println("failed to decode backend response:", err)
+            http.Error(w, "invalid response from backend", http.StatusBadGateway)
             return
         }
-
-        tmpl.Execute(w, fortunes)
-        return
+    
+        tmpl, err := template.ParseFiles("./templates/fortunes.html")
+        if err != nil {
+            log.Println(err)
+            http.Error(w, "failed to load template", http.StatusInternalServerError)
+            return
+        }
+    
+        if err := tmpl.Execute(w, fortunes); err != nil {
+            log.Println("failed to execute template:", err)
+        }
     })
 
     http.HandleFunc("/api/add", func (w http.ResponseWriter, r *http.Request) {
@@ -82,21 +101,25 @@ func main() {
         }
 
         f := new(newFortune)
-        json.NewDecoder(r.Body).Decode(f)
+        if err := json.NewDecoder(r.Body).Decode(f); err != nil {
+            log.Println("failed to decode request:", err)
+            http.Error(w, "invalid request", http.StatusBadRequest)
+            return
+        }
 
         var postUrl = fmt.Sprintf("http://%s:%s/fortunes", BACKEND_DNS, BACKEND_PORT)
         var jsonStr = []byte(fmt.Sprintf(`{"id": "%d", "message": "%s"}`, rand.Intn(10000), f.Message))
 
         _, err := myClient.Post(postUrl, "application/json", bytes.NewBuffer(jsonStr))
         if err != nil {
-            log.Fatalln(err)
-            fmt.Fprint(w, err)
+            log.Println(err)
+            http.Error(w, "backend unavailable", http.StatusBadGateway)
             return
         }
 
-        fmt.Fprint(w, "Cookie added!")
-
-        return
+        if _, err := fmt.Fprint(w, "Cookie added!"); err != nil {
+            log.Println("failed to write response:", err)
+        }
     })
 
     http.Handle("/", http.FileServer(http.Dir("./static")))
